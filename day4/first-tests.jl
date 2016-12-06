52fac92bd6bf7426042cc1fdd3872e9b8b04ffee
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2016-12-05 19:08:37 -0500

    Fix problem with day4 first problem

--- day4/first-tests.jl
+++ day4/first-tests.jl
@@ -2,7 +2,7 @@ using Base.Test
 
 include("first.jl")
 
-@test is_valid("aaaaa-bbb-z-y-x-123[abxyz]")
-@test is_valid("a-b-c-d-e-f-g-h-987[abcde]")
-@test is_valid("not-a-real-room-404[oarel]")
-@test !is_valid("totally-real-room-200[decoy]")
+@test is_valid("aaaaabbbzyx", "abxyz")
+@test is_valid("abcdefgh", "abcde")
+@test is_valid("notarealroom", "oarel")
+@test !is_valid("totallyrealroom", "decoy")
--- day4/first.jl
+++ day4/first.jl
@@ -10,14 +10,8 @@ function klargest(n::Int, v::DataStructures.Accumulator{Char, Int64})
     return Dict(v_keys[p_keys][idx] => v_vals[p_keys][idx] for idx in p_vals[1:n])
 end
 
-function is_valid(line)
-    left, check = split(line, "[")
-    chars_and_sector = split(left, "-")
-    sector = parse(Int, chars_and_sector[end])
-    string = join(chars_and_sector[1:end-1])
-
+function is_valid(string, check)
     counts = counter([char for char in string])
-
     return Set(keys(klargest(5, counts))) == Set(check[1:5])
 end
 
@@ -26,7 +20,12 @@ function solve(fname)
     tot = 0
 
     for line in readlines(fname)
-        if is_valid(chomp(line))
+        left, check = split(line, "[")
+        chars_and_sector = split(left, "-")
+        sector = parse(Int, chars_and_sector[end])
+        string = join(chars_and_sector[1:end-1])
+
+        if is_valid(string, check)
             tot += sector
         end
     end
