d135ece5c7b687d6002740f6e6ea9e28d453093d
Taylor Pospisil <tpospisi@andrew.cmu.edu>
2016-12-10 22:34:14 -0500

    Reduce length of day10 solution
    I need to have a shorter solution (within bounds of reasonableness)
    than my friend.

--- day10/first.jl
+++ day10/first.jl
@@ -6,42 +6,36 @@ function solve(lines)
 
     while !all(executed)
         for (ix, line) in enumerate(lines)
-            if !executed[ix]
-                parts = split(line, ' ')
-                if parts[1] == "value"
-                    value = parse(Int, parts[2])
-                    bot = parse(Int, parts[6])
+            !executed[ix] || continue
+            parts = split(line, ' ')
+            if parts[1] == "value"
+                value = parse(Int, parts[2])
+                bot = parse(Int, parts[6])
 
-                    length(chips[bot]) <= 2 || continue
-                    push!(chips[bot], value)
+                length(chips[bot]) <= 2 || continue
+                push!(chips[bot], value)
 
-                    if sort(chips[bot]) == [17, 61]
-                        return bot
-                    end
+                sort(chips[bot]) == [17, 61] && return bot
+            else
+                giver = parse(Int, parts[2])
+                low_getter = parse(Int, parts[7])
+                high_getter = parse(Int, parts[12])
 
-                    executed[ix] = true
-                else
-                    giver = parse(Int, parts[2])
-                    low_getter = parse(Int, parts[7])
-                    high_getter = parse(Int, parts[12])
+                length(chips[giver]) == 2 || continue
+                low, high = sort(chips[giver])
 
-                    length(chips[giver]) == 2 || continue
-                    low, high = sort(chips[giver])
+                length(chips[low_getter]) < 2 || continue
+                length(chips[high_getter]) < 2 || continue
 
-                    length(chips[low_getter]) < 2 || continue
-                    length(chips[high_getter]) < 2 || continue
+                push!(chips[low_getter], low)
+                push!(chips[high_getter], high)
+                chips[giver] = []
 
-                    push!(chips[low_getter], low)
-                    push!(chips[high_getter], high)
-                    chips[giver] = []
-
-                    for id in [low_getter, high_getter]
-                        sort(chips[low_getter]) == [17, 61] && return id
-                    end
-
-                    executed[ix] = true
+                for id in [low_getter, high_getter]
+                    sort(chips[id]) == [17, 61] && return id
                 end
             end
+            executed[ix] = true
         end
     end
 
--- day10/second.jl
+++ day10/second.jl
@@ -6,33 +6,30 @@ function solve(lines)
 
     while !all(executed)
         for (ix, line) in enumerate(lines)
-            if !executed[ix]
-                parts = split(line, ' ')
-                if parts[1] == "value"
-                    value = parse(Int, parts[2])
-                    bot = parse(Int, parts[6])
+            !executed[ix] || continue
+            parts = split(line, ' ')
+            if parts[1] == "value"
+                value = parse(Int, parts[2])
+                bot = parse(Int, parts[6])
 
-                    length(chips[bot]) <= 2 || continue
-                    push!(chips[bot], value)
-                    executed[ix] = true
-                else
-                    giver = parse(Int, parts[2])
-                    low_getter = parse(Int, parts[7])
-                    high_getter = parse(Int, parts[12])
+                length(chips[bot]) <= 2 || continue
+                push!(chips[bot], value)
+            else
+                giver = parse(Int, parts[2])
+                low_getter = parse(Int, parts[7])
+                high_getter = parse(Int, parts[12])
 
-                    length(chips[giver]) == 2 || continue
-                    low, high = sort(chips[giver])
+                length(chips[giver]) == 2 || continue
+                low, high = sort(chips[giver])
 
-                    length(chips[low_getter]) < 2 || continue
-                    length(chips[high_getter]) < 2 || continue
+                length(chips[low_getter]) < 2 || continue
+                length(chips[high_getter]) < 2 || continue
 
-                    push!(chips[low_getter], low)
-                    push!(chips[high_getter], high)
-                    chips[giver] = []
-
-                    executed[ix] = true
-                end
+                push!(chips[low_getter], low)
+                push!(chips[high_getter], high)
+                chips[giver] = []
             end
+            executed[ix] = true
         end
     end
 
